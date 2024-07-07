51c9022a91e3b1f356b4fe305785d33693abff32
Shane <140445981+digitalshane@users.noreply.github.com>
2024-07-07 17:54:48 -0400

    Update

--- global-head.js
+++ global-head.js
@@ -1,1 +1,36 @@
-testing
+// Import Pirsch.js
+(function() {
+    var script = document.createElement('script');
+    script.defer = true;
+    script.type = 'text/javascript';
+    script.src = 'https://api.pirsch.io/pirsch.js';
+    script.id = 'pirschjs';
+    script.setAttribute('data-code', 'oqa9i7714JAzObWnHCQEDL2PD5aq280g');
+    document.head.appendChild(script);
+  })();
+  
+  // Import Clarity
+  (function(c,l,a,r,i,t,y){
+    c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+    t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+    y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+  })(window, document, "clarity", "script", "fz3vn4s7wa");
+  
+  // Import Pirsch Extended
+  (function() {
+    var script = document.createElement('script');
+    script.defer = true;
+    script.type = 'text/javascript';
+    script.src = 'https://api.pirsch.io/pirsch-extended.js';
+    script.id = 'pirschextendedjs';
+    script.setAttribute('data-code', 'oqa9i7714JAzObWnHCQEDL2PD5aq280g');
+    document.head.appendChild(script);
+  })();
+  
+  // Import Humblytics
+  (function() {
+    var script = document.createElement('script');
+    script.async = true;
+    script.src = 'https://app.humblytics.com/hmbl.min.js?id=0e47a87';
+    document.head.appendChild(script);
+  })();
